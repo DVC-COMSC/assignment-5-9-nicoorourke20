219477b734fda4ab708a3ebe97b1e2fbc56b635d
nicoorourke20 <157660744+nicoorourke20@users.noreply.github.com>
2024-03-08 21:35:12 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,36 @@
-
 #include <iostream>
 using namespace std;
 
 int main()
 {
-	int prev, current;
-	/* ****************************
-	// Complete this code.
-	*************************** */
+    int prev, current;
+    bool first_input = true;
+
+    // Get the first input
+    cin >> prev;
+
+    // Loop until the current input is greater than the previous input
+    while (true) {
+        // Get the current input
+        cin >> current;
+
+        // Check if the current input is less than the previous input
+        if (current < prev) {
+            // Print the current input without newline
+            if (!first_input) {
+                cout << " ";
+            }
+            cout << current;
+            first_input = false;
+        } else {
+            // Print newline and stop the loop
+            cout << endl;
+            break;
+        }
+
+        // Update the previous input
+        prev = current;
+    }
+
+    return 0;
 }
